ed256abf676e9355df3722876225a832b1a8de20
Stephen Vaz <54773712+stephenvaz03@users.noreply.github.com>
2021-11-03 22:19:11 +0530

    Firebase Auth & User model complete

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,3 +1,28 @@
-class UserModel{
+class UserModel {
+  String? uid;
+  String? email;
+  String? name;
 
+  UserModel({
+    this.uid,
+    this.email,
+    this.name,
+  });
+
+  //recieve from firestore
+  factory UserModel.fromMap(map) {
+    return UserModel(
+      uid: map['uid'],
+      email: map['email'],
+      name: map['name'],
+    );
+  }
+  //send to firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'name': name,
+    };
+  }
 }
--- lib/reg.dart
+++ lib/reg.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:walp/main.dart';
+import 'package:walp/model/user_model.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -8,10 +13,13 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _auth = FirebaseAuth.instance;
+
   final formKey = GlobalKey<FormState>();
   final TextEditingController _name = TextEditingController();
   final TextEditingController _emailC = TextEditingController();
   final TextEditingController _passC = TextEditingController();
+  final TextEditingController _pass2C = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,15 +43,15 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
       validator: (value) {
-          RegExp regex = RegExp(r'^.{3,}$');
-          if (value!.isEmpty) {
-            return ("Name cannot be Empty");
-          }
-          if (!regex.hasMatch(value)) {
-            return ("Enter Valid name(Min. 3 Character)");
-          }
-          return null;
-        },
+        RegExp regex = RegExp(r'^.{3,}$');
+        if (value!.isEmpty) {
+          return ("Name cannot be Empty");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid name(Min. 3 Character)");
+        }
+        return null;
+      },
     );
     final email = TextFormField(
       style: const TextStyle(color: Colors.white),
@@ -65,16 +73,15 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
       validator: (value) {
-          if (value!.isEmpty) {
-            return ("Please Enter Your Email");
-          }
-          // reg expression for email validation
-          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-              .hasMatch(value)) {
-            return ("Please Enter a valid email");
-          }
-          return null;
-        },
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
     );
     final pass = TextFormField(
       style: const TextStyle(color: Colors.white),
@@ -97,18 +104,48 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
       validator: (value) {
-          RegExp regex = RegExp(r'^.{6,}$');
-          if (value!.isEmpty) {
-            return ("Password is required for login");
-          }
-          if (!regex.hasMatch(value)) {
-            return ("Enter Valid Password(Min. 6 Character)");
-          }
-        },
+        RegExp regex = RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Password(Min. 6 Character)");
+        }
+      },
+    );
+
+    final pass2 = TextFormField(
+      style: const TextStyle(color: Colors.white),
+      autofocus: false,
+      controller: _pass2C,
+      obscureText: true,
+      keyboardType: TextInputType.visiblePassword,
+      onSaved: (value) => _pass2C.text = value!,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(
+          Icons.vpn_key,
+          color: Colors.white,
+        ),
+        contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: 'Confirm Password',
+        hintStyle: const TextStyle(color: Colors.white),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+      validator: (value) {
+        if (_pass2C.text != _passC.text) {
+          return ("Password doesn't match");
+        }
+        return null;
+      },
     );
 
     final signUpButton = ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        signUp(_emailC.text, _passC.text);
+      },
       child: const Text('Sign Up'),
       style: ElevatedButton.styleFrom(
         primary: Colors.white.withOpacity(0.05),
@@ -154,6 +191,11 @@ class _SignUpState extends State<SignUp> {
                         padding: const EdgeInsets.all(8.0),
                         child: pass,
                       ),
+                      const Padding(padding: EdgeInsets.all(8.0)),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: pass2,
+                      ),
                       signUpButton,
                     ]),
               ),
@@ -162,5 +204,40 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
     );
+  }
+
+  void signUp(String email, String pass) async {
+    if (formKey.currentState!.validate()) {
+      await _auth
+          .createUserWithEmailAndPassword(email: email, password: pass)
+          .then((value) => {postDetailstoFirestore()})
+          .catchError((e) {
+        Fluttertoast.showToast(msg: e!.message);
+      });
+    }
+  }
+//post detail fun yet to check if it works, everything else seems to work
+  postDetailstoFirestore() async {
+    //call firestore
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    //fetch and assign to current user
+    User? user = _auth.currentUser;
+    //call user model
+    UserModel userModel = UserModel();
+
+    userModel.email = user!.email;
+    userModel.uid = user.uid;
+    userModel.name = _name.text;
+
+    await firebaseFirestore
+        .collection('users')
+        .doc(user.uid)
+        .set(userModel.toMap());
+    Fluttertoast.showToast(msg: 'Account Created Successfully');
+
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const Initial()),
+        (route) => false);
   }
 }
